a20bb18e68f0b26789ad68012a7f9edad60f2396
jiach123 <2667240960@qq.com>
2023-04-06 20:13:42 +0800

    day3

---
+++ shop-admin1/src/pages/home.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+     agfdddsfdfs
+    </div>
+</template>
--- shop-admin1/src/router/index.js
+++ shop-admin1/src/router/index.js
@@ -5,14 +5,19 @@ import {
 import Index from "~/pages/index.vue"
 import NotFound from "~/pages/404.vue"
 import Login from "~/pages/login.vue"
+import home from "~/pages/home.vue"
 const routers=[
    {
     path :'/',
     component:Index
    },
-   {
+   {   
     path :'/login',
     component:Login
+   },
+   {
+    path :'/home',
+    component:home
    },
    { 
     path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound 
